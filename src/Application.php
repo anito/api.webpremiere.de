764d31daaad0cfd13a785c3cb1e6269224fa06f4
anito <axelnitzschner@mac.com>
2022-11-05 13:07:05 +0100

    removed ozee31/cakephp-cors plugin

--- src/Application.php
+++ src/Application.php
@@ -49,7 +49,6 @@ class Application extends BaseApplication
     // Call parent to load bootstrap from files.
     parent::bootstrap();
 
-    $this->addPlugin('Cors');
     $this->addPlugin('Crud');
 
     if (PHP_SAPI === 'cli') {
